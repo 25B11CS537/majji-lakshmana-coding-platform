a59b54aed096d24c12165c7e7691a5b6f1744a5a
25B11CS537 <lakshmanamajji96@gmail.com>
2025-12-08 11:45:32 +0530

    Add files via upload

---
+++ sum of first and last digits.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,first,last;
+    scanf("%d",&n);
+    last = n % 10;
+    while (n>=10)
+    {
+        n =n/10;
+ }
+ first = n;
+ printf("%d",first + last);
+ return 0 ;
+}
